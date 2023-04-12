f5ab813419959294b79c02d5244dea8f118c6721
unknown <dautaz@mail.ru>
2023-04-12 21:05:02 +0600

    Lab9

--- Lab9/hh_back/api/models.py
+++ Lab9/hh_back/api/models.py
@@ -7,9 +7,27 @@ class Company(models.Model):
     city = models.CharField(max_length = 255)
     address = models.TextField()
 
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+
 class Vacancy(models.Model):
     id = models.IntegerField(primary_key = True)
     name = models.CharField(max_length = 255)
     description = models.TextField()
     salary = models.FloatField()
     company = models.ForeignKey(Company, on_delete = models.CASCADE)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company
+        }
